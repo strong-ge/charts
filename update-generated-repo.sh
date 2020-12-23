82d8bfbd00a4c1344f81a8fa18c6e4b97fbec904
zhujq <jianqiang.zhu@woqutech.com>
2020-12-23 16:15:59 +0800

    add github action

--- update-generated-repo.sh
+++ update-generated-repo.sh
@@ -2,11 +2,24 @@
 
 set -e
 
-build_dir="docs"
+build_dir="public"
+mkdir -p $build_dir
 cd $build_dir
 for chart in ../charts/*/; do
   if [[ -f "$chart"/Chart.yaml ]]; then
-    helm package $chart
+    helm package -u $chart
   fi
 done
 helm repo index --merge index.yaml --url https://grdscloud.github.io/charts/ ./
+
+cat > index.html <<EOF
+<html>
+<head>
+    <title>Chart repo</title>
+</head>
+<body>
+<h1>Grds Cloud Charts Repo</h1>
+<p>Point Helm at this repo to see charts.</p>
+</body>
+</html>
+EOF
